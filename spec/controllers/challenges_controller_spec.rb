0a0b97cacf518a894ded1ce226440a3ca4886df4
Suguru Odai <ourqwers@gmail.com>
2015-03-18 19:35:42 +0900

    Add test case for challenge controller

--- spec/controllers/challenges_controller_spec.rb
+++ spec/controllers/challenges_controller_spec.rb
@@ -1,12 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe ChallengesController, type: :controller do
+  let(:challenge) { create(:challenge) }
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
+  shared_examples '#fetch_challenges' do
+    it '@challengesに作成したchallengeが含まれていること' do
+      expect(assigns(:challenges)).to be_include(challenge)
+    end
+  end
+
+  shared_examples '#fetch_challenge' do
+    it '作成したchallengeが取得できていること' do
+      expect(assigns(:challenge)).to eq challenge
+    end
+  end
+
+  describe 'GET #index' do
+    before do
+      get :index, course_id: challenge.course
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it_behaves_like '#fetch_challenges'
+  end
+
+  describe 'GET #show' do
+    before do
+      get :show, course_id: challenge.course, sequence_number: challenge
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it_behaves_like '#fetch_challenge'
+  end
+
+  describe 'GET #new' do
+    before do
+      get :new, course_id: challenge.course
+    end
+
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
   end
 
+  describe 'POST #resolve' do
+    context 'テキストに誤りがあるリクエストだったとき' do
+      before do
+        post :resolve,
+          course_id: challenge.course,
+          sequence_number: challenge,
+          raw_text: 'foo'
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'correct が false であるデータを返すこと' do
+        expect(JSON.parse(response.body)['correct']).to be_falsy
+      end
+    end
+
+    context 'テキストに誤りがないリクエストだったとき' do
+      before do
+        post :resolve,
+          course_id: challenge.course,
+          sequence_number: challenge,
+          raw_text: challenge.en_text
+      end
+
+      it 'returns http success' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'correct が true であるデータを返すこと' do
+        expect(JSON.parse(response.body)['correct']).to be_truthy
+      end
+    end
+   end
+
+  describe 'POST #teach_partial_answer' do
+    before do
+      post :teach_partial_answer,
+        course_id: challenge.course,
+        sequence_number: challenge,
+        raw_text: 'foo'
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+  end
 end
