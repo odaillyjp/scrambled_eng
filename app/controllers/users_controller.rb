7a6fd71e7fb75fad1b95cf2ca8c3c085d354cf8f
Suguru Odai <ourqwers@gmail.com>
2015-04-25 15:58:13 +0900

    Add manageable courses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @histories = History.where(user: @user)
-                   .joins(:challenge)
-                   .includes(challenge: :course)
-                   .group('challenges.course_id')
-                   .limit(5)
+    @recently_histories = History.where(user: @user)
+                            .joins(:challenge)
+                            .includes(challenge: :course)
+                            .group('challenges.course_id')
+                            .limit(5)
+    @manageable_courses = Course.where(user: @user)
   end
 end
