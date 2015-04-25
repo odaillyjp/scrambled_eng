c42d825b477a90cc21e977cc47226360208699b8
Suguru Odai <ourqwers@gmail.com>
2015-04-25 15:35:24 +0900

    Add recently played course list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @histories = History.where(user: @user)
+                   .joins(:challenge)
+                   .includes(challenge: :course)
+                   .group('challenges.course_id')
+                   .limit(5)
   end
 end
