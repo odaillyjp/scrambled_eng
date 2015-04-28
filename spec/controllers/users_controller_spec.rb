03ee4a4d35cbc3c926fb06ff10ec2788876748ac
Suguru Odai <ourqwers@gmail.com>
2015-04-28 15:31:45 +0900

    Fix to user controller spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  let(:user) { create(:user) }
 
   describe 'GET #show' do
-    before { get :show }
+    before { get :show, id: user.id }
 
     it 'returns http success' do
       expect(response).to have_http_status(:success)
