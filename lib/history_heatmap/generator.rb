50763a7c23114772e0c48db3b54266ef452ca1c4
Suguru Odai <ourqwers@gmail.com>
2015-05-03 16:45:41 +0900

    Fix to #run with course_id

--- lib/history_heatmap/generator.rb
+++ lib/history_heatmap/generator.rb
@@ -16,74 +16,13 @@ module HistoryHeatmap
         from = params[:from] || Time.current
         to = params[:to] || Time.current
 
-        # NOTE: user_id, course_id 両パラメータを条件にした検索はできない.
-        #       両パラメータを条件にして検索するケースがなさそうなので, このようにしている.
-        query = if params[:user_id]
-                  heatmap_query_with_user_id(params[:user_id], from, to)
-                elsif params[:course_id]
-                  heatmap_query_with_course_id(params[:course_id], from, to)
-                end
-
-        ActiveRecord::Base.connection.select_all(query).rows.to_h.to_json
-      end
-
-      private
-
-      def heatmap_query_with_user_id(user_id, from, to)
-        query = case DB_ADAPTER
-                when 'sqlite3' then heatmap_query_with_user_id_for_sqlite
-                else fail 'unsupported adapter!'
-                end
-
-        ActiveRecord::Base.send(:sanitize_sql_array, [query, user_id, from, to])
-      end
-
-      def heatmap_query_with_course_id(course_id, from, to)
-        query = case DB_ADAPTER
-                when 'sqlite3' then heatmap_query_with_course_id_for_sqlite
-                else fail 'unsupported adapter!'
-                end
-
-        ActiveRecord::Base.send(:sanitize_sql_array, [query, course_id, from, to])
-      end
-
-      def heatmap_query_with_user_id_for_sqlite
-        <<-SQL
-          SELECT
-            STRFTIME('%s', updated_at) AS timestamp,
-            COUNT(*) AS value
-          FROM
-            histories
-          WHERE
-            (user_id = ?) AND
-            (updated_at BETWEEN ? AND ?)
-          GROUP BY
-            DATE(updated_at)
-          ;
-        SQL
-      end
-
-      def heatmap_query_with_course_id_for_sqlite
-        <<-SQL
-          SELECT
-            STRFTIME('%s', updated_at) AS timestamp,
-            COUNT(*) AS value
-          FROM
-            histories
-          WHERE
-            (challenge_id IN
-              (SELECT
-                challenge_id
-              FROM
-                challenges
-              WHERE
-                course_id = ?)
-            ) AND
-            (updated_at BETWEEN ? AND ?)
-          GROUP BY
-            DATE(updated_at)
-          ;
-        SQL
+        histories = History.where(updated_at: from..to)
+        histories = histories.where(user_id: params[:user_id]) if params[:user_id]
+        if params[:course_id]
+          histories = histories.joins(:challenge)
+                        .where(challenges: { course_id: params[:course_id] })
+        end
+        histories.group(:unix_timestamp).count
       end
     end
   end
