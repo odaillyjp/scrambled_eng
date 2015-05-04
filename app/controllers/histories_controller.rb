87c76bbc68efe439e1b4ba6928b008c357fac272
Suguru Odai <ourqwers@gmail.com>
2015-05-04 11:54:30 +0900

    Rename HistoryHeatmap::Generator to HistoryHeatmap::API

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,6 +1,6 @@
 class HistoriesController < ApplicationController
   def heatmap
-    json_data = HistoryHeatmap::Generator.run(params.extract!(:course_id, :user_id, :from, :to))
+    json_data = HistoryHeatmap::API.get(params.extract!(:course_id, :user_id, :from, :to))
     render json: json_data
   end
 end
