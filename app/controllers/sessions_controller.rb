e10f12fb5b8ac9804d1db3d66003981a0189e5dd
Suguru Odai <ourqwers@gmail.com>
2015-06-26 15:22:03 +0900

    Reset sessions before create

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,13 @@
 class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth_hash(auth_hash)
+    reset_session
     session[:user_id] = user.id
     redirect_to root_path
   end
 
   def destroy
-    session[:user_id] = nil
+    reset_session
     redirect_to root_path
   end
 
